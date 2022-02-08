7060b8a30ba6d81c1f777137fb71c7bf1a0bc75b
YassinEldeeb <yassineldeeb94@gmail.com>
2022-02-08 18:49:29 +0200

    feat: search

--- src/lib.rs
+++ src/lib.rs
@@ -32,13 +32,46 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut matches: Vec<&str> = vec![];
+
+    for line in content.lines() {
+        if line.contains(query) {
+            matches.push(line)
+        }
+    }
+
+    matches
+}
+
 #[cfg(test)]
-mod tests {
+mod search_functionality {
+    use super::*;
+
+    #[test]
+    fn it_searches() {
+        let query = "Rust";
+        let content = "\
+The most loved programming languages are:
+1. Typescript.
+2. the Rust programming langauge.
+3. Kotlin.
+        ";
+
+        assert_eq!(
+            search(query, content),
+            ["2. the Rust programming langauge."]
+        );
+    }
+}
+
+#[cfg(test)]
+mod config_struct {
     use crate::Config;
 
     #[test]
     fn not_enough_args() {
-        let args = [String::from("0"), String::from("1")];
+        let args = [0.to_string(), 1.to_string()];
         if let Err(msg) = Config::new(&args) {
             assert_eq!(msg, "not enough arguments")
         }
@@ -49,7 +82,7 @@ mod tests {
         let query = "word";
         let file_location = "hello.txt";
         let args = [
-            String::from("0"),
+            "0".to_string(),
             query.to_string(),
             file_location.to_string(),
         ];
